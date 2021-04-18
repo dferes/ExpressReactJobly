7885e50decfe9a38b5af93c7d4269fa0b0fa54d4
Dylan Feres <dferes23@gmail.com>
2021-04-17 23:13:06 -0700

    added some ste to to load of all jobs and all companies when the app first loads

--- src/App.js
+++ src/App.js
@@ -1,15 +1,35 @@
+import React, { useState , useEffect } from 'react';
 import './App.css';
 import { BrowserRouter } from 'react-router-dom';
 import NavBar from './NavBar';
 import Routes from './Routes';
+import JoblyApi from './api';
 
 function App() {
+  const [ companies, setCompanies ] = useState([]);
+  const [ jobs, setJobs ] = useState([]);
+  
+  useEffect( () => {
+    const getCompanies = async () => {
+      const allCompanies = await JoblyApi.getAllCompanies();
+      setCompanies(allCompanies);
+    }
+
+    const getJobs = async () => {
+      const allJobs = await JoblyApi.getAllJobs();
+      setJobs(allJobs);
+    }
+
+    getCompanies();
+    getJobs();
+  }, []);
+
   return (
     <div className="App">
       <BrowserRouter>
         <NavBar />
         <main>
-          <Routes />
+          <Routes companies={companies} jobs={jobs} />
         </main>
       </BrowserRouter>
     </div>
