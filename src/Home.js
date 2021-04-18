dd2f7c30dfb864e839183a19b0d4de95d06f3aa7
Dylan Feres <dferes23@gmail.com>
2021-04-17 23:18:38 -0700

    hardcoded state for mock

--- src/Home.js
+++ src/Home.js
@@ -2,15 +2,22 @@ import React from 'react';
 import './Home.css';
 import { Button } from 'reactstrap';
 
-const Home = () => {
-
+const Home = ({ isLoggedIn=false }) => {
+// TODO:  will need to include logged in user's name in the welcome back message
   return (
     <section>
       <div className='jobly-welcome-div'>
         <h1 className='jobly-welcome-title'>Jobly</h1>
         <h3 className='jobly-welcome-message'>All the jobs in one, convenient place.</h3>
-        <Button color='primary' className='login-button'>Login</Button>
-        <Button color='primary' className='signup-button'>Signup</Button>
+        { !isLoggedIn &&
+          <Button color='primary' className='login-button'>Login</Button>
+        }    
+        { !isLoggedIn &&
+          <Button color='primary' className='signup-button'>Signup</Button>
+        } 
+        {
+          isLoggedIn && <h2>Welcome Back, {}!</h2>  
+        }  
       </div>  
     </section>
   );
