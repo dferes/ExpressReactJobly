c7637bae6cb2b7601c91fc6feb3c2481dcf606e7
Dylan Feres <dferes23@gmail.com>
2021-04-18 01:47:33 -0700

    made a card-like div to display companies

--- src/CompanyList.js
+++ src/CompanyList.js
@@ -1,14 +1,19 @@
 import React from 'react';
+import './CompanyList.css';
+import { Link } from 'react-router-dom';
+
 
 const CompanyList = ({ companies }) => {
-
-  return (
+  return (  
     <div>
-      <h2>companyList, mang</h2>
-      {companies.map( com => (
-        <div key={com.handle}> 
-          <p>{com.handle}</p> 
-        </div>  
+      {companies.map( comp => (
+        <Link className='company-link' to={`/companies/${comp.handle}`} key={comp.handle}>
+          <div className='company-div'> 
+            <p className='company-name'>{comp.name}</p>
+            {/* <img className='company-image' src={comp.logoUrl} alt=''/> */}
+            <p className='company-description'>{comp.description}</p> 
+          </div>  
+        </Link>  
       ))}  
     </div>  
   );
