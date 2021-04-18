97f5b41ddba05eaa1f840ec2045197c4f62a0e67
Dylan Feres <dferes23@gmail.com>
2021-04-17 23:14:33 -0700

    checking that the props passed from the App component are coming through

--- src/JobList.js
+++ src/JobList.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-const JobList = () => {
-    
+const JobList = ({ jobs }) => {
+
   return (
     <div>
-      <h2>jobList, yo</h2>  
+      <h2>jobList, bruh</h2>
+      {jobs.map( job => (
+        <div key={job.id}> 
+          <p>{job.id}</p>
+          <Link to={`/jobs/${job.id}`}>{job.title}</Link>   
+        </div>  
+      ))}  
     </div>  
   );
 };
