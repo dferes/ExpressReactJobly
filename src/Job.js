c98db45bf0869c142d53a422d2e366a7370008eb
Dylan Feres <dferes23@gmail.com>
2021-04-18 20:29:02 -0700

    Moved all card logic from JobList into this file

--- src/Job.js
+++ src/Job.js
@@ -1,18 +1,18 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
+import { Button } from 'reactstrap';
 import './Job.css';
 
 
-const Job = ({ jobs }) => {
-  const { id } = useParams();
-  const job = jobs.find( jb => +id === jb.id);
-
+const Job = ({ id, title, companyHandle, salary, equity }) => {
   return (
-    <div>
-      <h2 className='a-job-title'>{job.title}</h2>
-      <p className='a-job-salary'>Salary: ${job.salary}</p>
-      <p className='a-job-equity'> Equity: {job.equity}</p>
-    </div>  
+    <div className='job-div' key={id}> 
+      <p className='job-title'>{title}</p>
+      <p className='job-company-handle'>{companyHandle}</p>
+      {/* <img className='company-image' src={comp.logoUrl} alt=''/> */}
+      <p className='job-salary'>Salary: ${salary}</p>
+      <p className='job-equity'>Equity: {equity? equity: 0}</p>
+      <Button className='job-apply-button' color='danger'>Apply</Button>
+  </div>  
   );  
 }
 
