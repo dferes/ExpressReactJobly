7b2d645b83008ed0623eb81b72da1b47ecc6a7ad
Dylan Feres <dferes23@gmail.com>
2021-04-18 20:29:24 -0700

    Moved all card logic into Job.js

--- src/JobList.js
+++ src/JobList.js
@@ -1,24 +1,38 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import { Button } from 'reactstrap';
 import './JobList.css';
-
+import Job from './Job';
 
 const JobList = ({ jobs }) => {
+
+//   const [ formData, setFormData ] = useState({ name: ''});
+//   const [ jobList, setJobList ] = useState(companies);
+//   const [ isEmpty, setIsEmpty ] = useState(false);
+    
+//   const handleChange = evt => {
+//     const { name, value } = evt.target;  
+//     setFormData( data => ({...data, [name]: value}));
+//   };
+  
+//   const handleSubmit = async evt => {
+//     evt.preventDefault();  
+//     const res = formData.name !== '' ?
+//       await JoblyApi.getAllCompanies(formData)
+//       : companies;
+//     setIsEmpty( res.length === 0 ? true: false); 
+//     setCompanyList(res);
+//   };
 
   return (
     <div>
       {jobs.map( job => (
-        <Link className='job-link' to={`/jobs/${job.id}`} key={job.id}>
-          <div className='job-div'> 
-            <p className='job-title'>{job.title}</p>
-            <p className='job-company-handle'>{job.companyHandle}</p>
-            {/* <img className='company-image' src={comp.logoUrl} alt=''/> */}
-            <p className='job-salary'>Salary: ${job.salary}</p>
-            <p className='job-equity'>Equity: {job.equity? job.equity: 0}</p>
-            <Button className='job-apply-button' color='danger'>Apply</Button>
-          </div>  
-        </Link>  
+        <Job 
+          key={job.id}
+          id={job.id}
+          title={job.title}
+          companyHandle={job.companyHandle}
+          salary={job.salary}
+          equity={job.equity}
+        />
       ))}  
     </div>  
   );
