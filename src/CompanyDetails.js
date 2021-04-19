df21e70b0f3be3b38a6c59e3886433c90ca63ee3
Dylan Feres <dferes23@gmail.com>
2021-04-18 20:26:55 -0700

    a page that shows company name and description, aswell as a list of all available jobs associated with said company

---
+++ src/CompanyDetails.js
@@ -0,0 +1,45 @@
+import React, { useEffect, useState } from 'react';
+import Job from './Job';
+import { useParams } from 'react-router-dom';
+import JoblyApi from './api';
+import './CompanyDetails.css';
+
+const CompanyDetails = () => {
+  const [ company, setCompany ] = useState({});
+  const [ readyToRender, setReadyToRender ] = useState(false);
+  const { handle } = useParams();
+  
+  useEffect( async () => {
+    const getCompanyJobs = async () => {
+      const company = await JoblyApi.getCompany(handle);
+      setCompany(company);
+      setReadyToRender(true);
+    };
+    
+    getCompanyJobs();
+  }, []);
+  
+
+  return (
+    <div className='company-jobs-div'>
+      { readyToRender && 
+        <div className='company-details-div'>
+          <h4 className='company-details-title'>{company.name}</h4>
+          <p className='company-details-description'>{company.description}</p>
+        </div>  
+      }  
+      { readyToRender && company.jobs.map( job => (
+        <Job 
+          key={job.id}
+          id={job.id}
+          title={job.title}
+          companyHandle={job.companyHandle}
+          salary={job.salary}
+          equity={job.equity}
+        />  
+      ))}
+    </div>
+  );  
+};
+
+export default CompanyDetails;
