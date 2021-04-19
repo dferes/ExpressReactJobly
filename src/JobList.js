c1a0851284a0d7b4e2227a46edf1389789767666
Dylan Feres <dferes23@gmail.com>
2021-04-18 21:26:14 -0700

    Added state and some conditional logic for rendering No Result message and form for searching job titles

--- src/JobList.js
+++ src/JobList.js
@@ -1,30 +1,48 @@
-import React from 'react';
-import './JobList.css';
+import React, { useState } from 'react';
 import Job from './Job';
+import JoblyApi from './api';
+import { Button } from 'reactstrap';
+import './JobList.css';
+
 
 const JobList = ({ jobs }) => {
 
-//   const [ formData, setFormData ] = useState({ name: ''});
-//   const [ jobList, setJobList ] = useState(companies);
-//   const [ isEmpty, setIsEmpty ] = useState(false);
-    
-//   const handleChange = evt => {
-//     const { name, value } = evt.target;  
-//     setFormData( data => ({...data, [name]: value}));
-//   };
+  const [ formData2, setFormData2 ] = useState({ title: ''});
+  const [ jobList, setJobList ] = useState(jobs);
+  const [ isEmpty, setIsEmpty ] = useState(false);
   
-//   const handleSubmit = async evt => {
-//     evt.preventDefault();  
-//     const res = formData.name !== '' ?
-//       await JoblyApi.getAllCompanies(formData)
-//       : companies;
-//     setIsEmpty( res.length === 0 ? true: false); 
-//     setCompanyList(res);
-//   };
+  const handleChange2 = evt => {
+    const { name, value } = evt.target;  
+    setFormData2( data => ({...data, [name]: value}));
+  };
+  
+  const handleSubmit2 = async evt => {
+    evt.preventDefault();  
+    const res = formData2.title !== '' ?
+      await JoblyApi.getAllJobs(formData2)
+      : jobs;
+    setIsEmpty( res.length === 0 ? true: false); 
+    setJobList(res);
+    console.log('----->>><<< The RESULT IS: ', res);
+  };
 
   return (
     <div>
-      {jobs.map( job => (
+      <div className='job-form-div'>
+        <form onSubmit={handleSubmit2}>
+          <label htmlFor='job-title-search' />
+          <input 
+            className='job-title-search-input'
+            id='job-title-search'
+            name='title'
+            placeholder='Enter a serch term...'
+            value={formData2.title}
+            onChange={handleChange2}
+          />
+          <Button className='job-title-search-button' color='primary'>Submit</Button>    
+        </form>
+      </div>    
+      { !isEmpty && jobList.map( job => (
         <Job 
           key={job.id}
           id={job.id}
@@ -33,7 +51,8 @@ const JobList = ({ jobs }) => {
           salary={job.salary}
           equity={job.equity}
         />
-      ))}  
+      ))}
+      { isEmpty && <p className='no-results-msg'>Sorry, no results were found!</p> }  
     </div>  
   );
 };
