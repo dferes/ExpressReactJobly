568e153ad0f171d86296d274ec283d02aa4d1d3a
Dylan Feres <dferes23@gmail.com>
2021-04-19 22:33:38 -0700

    Added a form. No state added yet

--- src/Signup.js
+++ src/Signup.js
@@ -1,9 +1,66 @@
 import React from 'react';
+import { Form, FormGroup, Label, Input, Button } from 'reactstrap';
+import './Signup.css';
+
 
 const Signup = () => {
   return (
-    <div>
-      <h2>Sign up my brother</h2>  
+    <div className='signup-form-div'>
+      <h2 className='signup-message'>Sign Up</h2>
+      <Form className='signup-form'>
+        <FormGroup>
+          <Label for='username'>Username</Label>
+            <Input 
+              autoFocus 
+              type='text'
+              name='username'
+              id='username'
+              // value={formData.username}
+              // onChange={handleLoginChange}  
+            />
+        </FormGroup>
+        <FormGroup>
+          <Label for='password'>Password</Label>
+            <Input 
+              type='password'
+              name='password'
+              id='password'
+              // value={formData.username}
+              // onChange={handleLoginChange}  
+          />
+        </FormGroup>
+        <FormGroup>
+          <Label for='firstName'>First Name</Label>
+            <Input  
+              type='text'
+              name='firstName'
+              id='firstName'
+              // value={formData.username}
+              // onChange={handleLoginChange}  
+          />
+        </FormGroup>
+        <FormGroup>
+          <Label for='lastName'>Last Name</Label>
+            <Input 
+              type='text'
+              name='lastName'
+              id='lastName'
+              // value={formData.username}
+              // onChange={handleLoginChange}  
+          />
+        </FormGroup>
+        <FormGroup>
+          <Label for='email'>Email</Label>
+            <Input 
+              type='email'
+              name='email'
+              id='email'
+              // value={formData.username}
+              // onChange={handleLoginChange}  
+            />
+        </FormGroup>
+        <Button color='primary' className='signup-form-button'>Sign Up</Button>
+      </Form>
     </div>  
   );  
 }
