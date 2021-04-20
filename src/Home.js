8242222a7b839a92eb76200f024c9bbf7d1f4005
Dylan Feres <dferes23@gmail.com>
2021-04-19 18:24:53 -0700

    Added the prop 'user' and new show the logged in username in the welcome back message

--- src/Home.js
+++ src/Home.js
@@ -2,21 +2,21 @@ import React from 'react';
 import './Home.css';
 import { Button } from 'reactstrap';
 
-const Home = ({ isLoggedIn }) => {
+const Home = ({ userToken, user }) => {
 
   return (
     <section>
       <div className='jobly-welcome-div'>
         <h1 className='jobly-welcome-title'>Jobly</h1>
         <h3 className='jobly-welcome-message'>All the jobs in one, convenient place.</h3>
-        { !isLoggedIn &&
-          <Button color='primary' className='login-button'>Login</Button>
+        { !userToken &&
+          <div>
+            <Button color='primary' className='homepage-login-button'>Login</Button>
+            <Button color='primary' className='homepage-signup-button'>Signup</Button>
+          </div>
         }    
-        { !isLoggedIn &&
-          <Button color='primary' className='signup-button'>Signup</Button>
-        } 
         {
-          isLoggedIn && <h2 className='jobly-hello-message'>Welcome Back, {}!</h2>  
+          userToken && <h2 className='jobly-hello-message'>Welcome Back, {user.firstName}!</h2>  
         }  
       </div>  
     </section>
