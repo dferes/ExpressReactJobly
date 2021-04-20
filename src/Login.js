66030fd3463ba76240e44314fd90bef6db60c9ea
Dylan Feres <dferes23@gmail.com>
2021-04-19 18:29:07 -0700

    Added a function reference 'setUser' as a prop to update the logged in user information with the state kept in 'App.js'. Added a function ref 'setUserToken' from a prop to update the logged in user token. Successful login credentials will redirect the user to the homepage.

--- src/Login.js
+++ src/Login.js
@@ -1,11 +1,25 @@
 import React, { useState } from 'react';
 import { Form, Button, FormGroup, Label, Input } from 'reactstrap';
+import { useHistory } from 'react-router-dom';
 import JoblyApi from './api';
 import './Login.css';
 
 
-const Login = () => {
+const Login = ({ setUserToken, setIsLoggedIn, setUser  }) => {
   const [ formData, setFormData ] = useState({ username: '', password: ''});
+  const [ validCredentials, setValidCredentials ] = useState(true);
+  const history = useHistory();
+
+  const setUserInfo = user => {
+    setUser({
+      username:     user.username,
+      firstName:    user.firstName,
+      lastName:     user.lastName,
+      email:        user.email,
+      isAdmin:      user.isAdmin,
+      applications: user.applications
+    });
+  }
 
   const handleLoginChange = evt => {
     const { name, value } = evt.target;
@@ -14,9 +28,19 @@ const Login = () => {
 
   const handleLoginSubmit = async evt => {
     evt.preventDefault();
-    let res = await JoblyApi.logIn(formData.username, formData.password,);
-    console.log('Login Response: ----->', res);
-    // probaly redirect from here if response isn't an error...either hompage or profile
+    try {
+      let token = await JoblyApi.logIn(formData.username, formData.password);
+      setUserToken(token);
+      setValidCredentials(true);
+     
+      const user = await JoblyApi.getUser(formData.username);
+      setUserInfo(user);
+      setIsLoggedIn(true);
+      history.push('/');
+    }catch(err) {
+      setValidCredentials(false);
+    }
+
   };
 
   return (
@@ -34,7 +58,6 @@ const Login = () => {
               onChange={handleLoginChange}  
             />
         </FormGroup>
-
         <FormGroup>
           <Label for='password'>Password</Label>
             <Input 
@@ -45,6 +68,11 @@ const Login = () => {
               onChange={handleLoginChange}  
             />
         </FormGroup>
+        { !validCredentials && 
+          <div className='bad-login-div'>
+            <p className='bad-login-message'>Invalid username/password</p>
+          </div>      
+        }
         <Button color='primary' className='login-button'>Log In</Button>
       </Form>
     </div>  
